15941c51df32114e25b70f4dbe067399c458babe
Noam Blanks <noamblanks@gmail.com>
2021-08-16 14:32:16 -0400

    commit'

--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  const { dogs } = props
+
+  const loading = () => {
+    return (
+      <h1> Loading... </h1>
+    )
+  }
+
+  const loaded = () => (
+    <div style={{ textAlign: "center" }}>
+      {dogs.map(( dog ) => (
+        <article>
+          <img src={ dog.img } />
+          <h1>{ dog.name }</h1>
+          <h3>Age: { dog.age }</h3>
+          <button onClick={() => {
+            props.selectDog( dog )
+            props.history.push( "/edit" )}}>
+            Edit
+          </button>
+          <button style={{"background-color": "red"}}onClick={() => {
+            props.deleteDog( dog )
+            props.history.push( "/" )}}>
+            Delete
+          </button>
+        </article>
+      ))}
+    </div>
+  )
+
+  return dogs.length > 0 ? loaded() : loading()
 };
 
 export default Display;
